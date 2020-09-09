0eedd3ce8e4bce26926192c28e626674fe3ba756
YingdaXia <xiayingda1994@gmail.com>
2020-09-09 14:18:59 -0400

    fix path

--- spade-cityscapes/run.sh
+++ spade-cityscapes/run.sh
@@ -1,8 +1,8 @@
 python train.py --name cityscapes --dataset_mode cityscapes \
-                --dataroot ./datasets/cityscapes \
+                --dataroot ../datasets/cityscapes \
                 --label_nc 19 --no_instance \
                 --niter 50 \
                 --batchSize 12 \
                 --nThread 16 \
-                --gpu_ids YOUR_GPU_IDS \
+                --gpu_ids 0,1,2,3 \
                 --no_html --tf_log
